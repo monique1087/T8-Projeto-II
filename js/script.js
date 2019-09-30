789958bd8398a79fe4f7151419b3abf8fb697435
Regiane <regianemaioli@hotmail.com>
2019-09-30 16:28:28 -0300

    inicio do projeto II

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// const caixinha = document.getElementById("input__text");
+// let inputTarefa = document.getElementById("idTarefaInput")
+
+// // let btn = addEventListener()
+
+// caixinha.addEventListener("click", function () {
+//     const valorInput = inputTarefa.Value;
+//     console.log(valorInput)
+//     const tarefaUm
+//   })
+
+const titulo = document.createElement("h2")
+const caixinha = document.getElementById("tarefa__catalogo")
+caixinha.appendChild(titulo)
+titulo.textContent = ("Adicionar nova Tarefa")
+
+const lista = docmente.createElement("li");
+caixinha.appendChild(lista)
+
+const tarefa = document.createElement("p")
+caixinha.appendChild(tarefa)
+
+const btn = document.createElement("button")
+caixinha.appendChild(btn)
+btn.textContent = "Feito"
